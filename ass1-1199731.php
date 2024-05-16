e66ff39a9151b5c90f025ad1e6056853fbce6c9e
bhumipatel1894 <48902835+bhumipatel1894@users.noreply.github.com>
2024-05-16 14:40:54 -0400

    init chnages

---
+++ ass1-1199731.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Assignemnt 1</title>
+</head>
+<body>
+	<h2>INFO-6096-(32)-24S</h2>
+	<p>Assignement - Storage systems</p>
+	<p>Hello World... Welcome to this page.</p>
+
+</body>
+</html>
